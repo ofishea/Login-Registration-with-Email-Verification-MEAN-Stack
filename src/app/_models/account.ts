9fc9170dda34189931765e0ef85951b433aa1483
ofishea <fishflowbean@gmail.com>
2022-10-19 18:28:22 +0100

    Add files via upload

---
+++ src/app/_models/account.ts
@@ -0,0 +1,17 @@
+﻿import { Role } from './role';
+
+
+export class Account {
+    id: string;
+    firstName: string;
+    lastName: string;
+    userName: string;
+    phone: string;
+    fiatBalance: Number;
+    newsletter: Boolean;
+    biometric: Boolean;
+    country: string;
+    email: string;
+    role: Role;
+    jwtToken?: string;
+}
---
+++ src/app/_models/index.ts
@@ -0,0 +1,6 @@
+﻿export * from './account';
+export * from './alert';
+export * from './role';
+export * from './wallet';
+export * from './bank';
+export * from './p2p';
